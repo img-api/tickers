4f3a19c541305e0d475336989672e048693c7294
Manel Villar <manelvf@gmail.com>
2025-01-23 16:10:11 +0000

    Load tickers instead of recipes

--- src/whats_for_dinner/main.py
+++ src/whats_for_dinner/main.py
@@ -1,20 +1,46 @@
 """Main server for API entrypoint"""
+import os
+import json
 import uvicorn
 from fastapi import FastAPI
+from google.oauth2 import service_account
 
-from .recipe import Recipe, load_recipes
+from recipe import Ticker, load_tickers
 
 
-recipes = load_recipes()
-recipe = Recipe(recipes)
+recipes = load_tickers()
+recipe = Ticker(recipes)
 
 app = FastAPI()
+
+
+credentials = json.load(open("/home/dev/vertex-ai-config.json"))
+google_credentials = credentials["GOOGLE_APPLICATION_CREDENTIALS"]
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials
+
+
+with open(google_credentials, 'r') as source:
+    info = json.load(source)
+
+
+# Auth using service account with json credentials
+service_account.Credentials.from_service_account_info(info)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 @app.get("/recommend_recipe")
 async def root(ingredients: str | None = "", image: int | None = None):
     if image:
         image_ingredients = recipe.get_image_ingredients(image)
+    else:
+        image_ingredients = ""
     ingredients = f"{ingredients}, {image_ingredients}"
 
     return recipe.get_recipe(ingredients)
@@ -22,4 +48,4 @@ async def root(ingredients: str | None = "", image: int | None = None):
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("my_package.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("my_package.main:app", host="0.0.0.0", port=5521, reload=True)
--- src/whats_for_dinner/recipe.py
+++ src/whats_for_dinner/recipe.py
@@ -1,17 +1,21 @@
 import os
 import os.path
+import json
 
-from .custom_components import ExtractFoodItemsFromImage
+from custom_components import ExtractFoodItemsFromImage
 
 from dotenv import load_dotenv
 from haystack import Document, Pipeline
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
-from haystack.components.generators.openai import OpenAIGenerator
-from haystack import component
+# from haystack.components.generators.openai import OpenAIGenerator
+from haystack_integrations.components.generators.google_vertex import VertexAIGeminiGenerator
 from haystack.components.builders import PromptBuilder
 from haystack_integrations.document_stores.pgvector import PgvectorDocumentStore
 from haystack_integrations.components.retrievers.pgvector import PgvectorKeywordRetriever
 from haystack.components.builders.answer_builder import AnswerBuilder
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/dev/vertex-ai-config.json"
 
 
 # Load environment variables
@@ -20,19 +24,22 @@ load_dotenv()
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 BASE_PATH = os.environ.get("BASE_PATH", "../../data")
-RECIPES_PATH = os.path.join(BASE_PATH, "recipes")
+TICKERS_PATH = os.path.join(BASE_PATH, "tickers")
 PHOTOS_PATH = os.path.join(BASE_PATH, "example_food_photos")
 
 
-class Recipe:
+class Ticker:
     def __init__(self, recipes: str):
         # Initialize PostgreSQL Document Store
+        print("INITIALIZING DOCUMENT STORE")
+
         self.document_store = PgvectorDocumentStore(
             embedding_dimension=768,
             vector_function="cosine_similarity",
             recreate_table=True,
             search_strategy="hnsw",
         )
+        print("DOCUMENT STORE INITIALIZED")
 
         self.insert_documents(recipes)
 
@@ -48,7 +55,7 @@ class Recipe:
 
         {% endfor %}
 
-        Question: Could you give me a recipe that contains most of these ingredients: {{ ingredients }}?
+        Question: Could you extract all the stock tickers from the context article?
         Add the instructions for that recipe, formatted as markdown.
         """
         pipe = Pipeline()
@@ -56,12 +63,12 @@ class Recipe:
         retriever = PgvectorKeywordRetriever(document_store=self.document_store)
         pipe.add_component("retriever", retriever)
         pipe.add_component(instance=PromptBuilder(template=template), name="prompt_builder")
-        pipe.add_component(instance=OpenAIGenerator(), name="llm")
+        pipe.add_component(instance=VertexAIGeminiGenerator(), name="llm")
         pipe.add_component(instance=AnswerBuilder(), name="answer_builder")
         pipe.connect("retriever", "prompt_builder.documents")
         pipe.connect("prompt_builder", "llm")
         pipe.connect("llm.replies", "answer_builder.replies")
-        pipe.connect("llm.meta", "answer_builder.meta")
+        # pipe.connect("llm.meta", "answer_builder.meta")
         pipe.connect("retriever", "answer_builder.documents")
 
         result = pipe.run(
@@ -76,9 +83,17 @@ class Recipe:
         return recipes
 
     def insert_documents(self, contents):
+        print("INSERTING DOCUMENTS")
+
         for content in contents:
             doc = Document(content=content, embedding=self.generate_doc_embedding(content))
-            self.document_store.write_documents([doc])
+            print("DOCUMENT EMBEDDING GENERATED")
+
+            try:
+                self.document_store.write_documents([doc])
+                print("DOCUMENT INSERTED")
+            except Exception as e:
+                print(e)
 
         print("DOCUMENTS INSERTED")
 
@@ -106,31 +121,46 @@ class Recipe:
         return result["image_extractor"]["answer"]
 
 
-def test_recipe():
-    recipes = load_recipes()
-    manager = Recipe(recipes)
+def test_ticker():
+    recipes = load_tickers()
+    manager = Ticker(recipes)
 
     user_ingredients = "chicken"
     manager.get_recipes(user_ingredients)
 
 
-def load_recipes():
-    print("RECIPES")
+def load_tickers():
+    print("loading RECIPES")
 
-    recipes = []
+    tickers = []
 
-    for f in os.listdir(RECIPES_PATH):
-        full_path = os.path.join(RECIPES_PATH, f)
+    for f in os.listdir(TICKERS_PATH):
+        full_path = os.path.join(TICKERS_PATH, f)
         if not os.path.isfile(full_path):
             continue
 
         with open(full_path) as f:
-            recipe = f.read()
+            # read the file as json
+            companies = json.load()
 
-        recipes.append(recipe)
+            for company in companies["companies"]:
+                tickers.append(
+                    """
+                    Company name: {company_name}
+                    Exchange tickers: {exchange_tickers},
+                    Related exchange tickers: {related_exchange_tickers}
+                    """.format(
+                        company_name=company["company_name"],
+                        exchange_tickers=f"{','.join(company['exchange_tickers'])}"
+                        related_exchange_tickers=f"{','.join(company['related_exchange_tickers'])}"
 
-    return recipes
+                    )
+                )
+
+    print(f"{len(tickers)} TICKERS loaded")
+
+    return tickers
 
 
 if __name__ == '__main__':
-    test_recipe()
+        test_ticker()
